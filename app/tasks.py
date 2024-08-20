7c5169164a331befbef297ce4b6131a06cddbdbb
Jitesh <sahujitesh101@gmail.com>
2024-08-21 00:32:37 +0530

    batch task using chunks

--- app/tasks.py
+++ app/tasks.py
@@ -1,6 +1,21 @@
-from celery import shared_task
+from celery import shared_task, chunks
 import time
+from django.utils import timezone
+from time import sleep
 
+
+@shared_task
+def process_pdf(pdf_path):
+    print(f"Processing PDF: {pdf_path}")
+    print(f"Processing time: {timezone.now().time()}")
+    sleep(3)
+
+@shared_task
+def s3_pro():
+    pdf_paths = [[f"pdf_{i}.pdf"] for i in range(1, 101)]
+    batch_size=10
+    # chunk = process_pdf.chunks(pdf_paths, 10)()
+    chunk = process_pdf.chunks(zip(pdf_paths), batch_size)().apply_async()
 
 
 @shared_task
